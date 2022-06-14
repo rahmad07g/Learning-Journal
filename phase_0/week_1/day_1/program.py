f4c3c3b3b5f98b721d3484c94fee8007c25e598c
Rahmad Gunawan <rahmad07g@gmail.com>
2022-06-15 00:35:47 +0700

    update baru

--- phase_0/week_1/day_1/program.py
+++ phase_0/week_1/day_1/program.py
@@ -1,6 +1,15 @@
+from re import X
+
+
 nama = input ("Masukkan nama anda :")
 usia = input ("Masukkan usia anda :")
 print (f"nama kamu adalah : {nama}")
 print (f"dan usia kamu adalah {usia}")
 
 # boleh menggunakan /n atau menggunakan """ (triple quote) atau bisa menggunakan double print
+
+
+# variable
+x = "hallo"
+
+print(x)
